6ad03e560a36f421d44c4f73a8942c598440d779
takaishi keito <keitoncis@icloud.com>
2018-05-23 19:07:33 +0900

    update

---
+++ Assets/Scenes/NoiseStudy/Scripts/Uniform.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Uniform : MonoBehaviour
+{
+	public Material mat;
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+		mat.SetFloat("_time",Time.realtimeSinceStartup);
+	}
+}
